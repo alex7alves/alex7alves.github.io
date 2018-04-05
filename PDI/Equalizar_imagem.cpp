47eb11fb9192458dcd785fc40825cc5516856e0c
alex7alves <alex7alves10@hotmail.com>
2018-04-05 10:44:30 -0300

    Atualizando sessão 4.1 e 5

---
+++ PDI/Equalizar_imagem.cpp
@@ -0,0 +1,39 @@
+
+/* Programa para equalizar o histograma
+   de uma imagem em tons de cinza
+
+   Autor : Alex Alves
+*/   
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv){
+
+    Mat image,cinza,equalizar;
+    image = imread("img2.jpg");
+    VideoCapture cap;
+    bool uniform = true,acummulate = false;
+    int nbins = 256;
+    float range[] = {0, 256};
+    const float *histrange = { range };
+
+    cvtColor(image,cinza,CV_BGR2GRAY);
+
+    // Calculando o histograma para tons de cinzas
+    calcHist(&cinza, 1, 0, Mat(),equalizar, 1,&nbins, &histrange,uniform,acummulate);
+
+    // Equalizando o histograma
+    equalizeHist(cinza,equalizar);
+    imshow("Cinza",cinza);
+    imshow("Equalizar",equalizar);
+    imwrite("original.jpg", cinza);
+    imwrite("equalizado.jpg",equalizar);
+    waitKey();
+   
+    return 0;
+}
