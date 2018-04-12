562da85cc72f92a042ad2ebe83bc7597321b0148
alex7alves <alex7alves10@hotmail.com>
2018-04-12 08:56:42 -0300

    Adicionando equalização de imagens coloridas

---
+++ PDI/Equalizar_colorido.cpp
@@ -0,0 +1,34 @@
+
+/* Programa para equalizar o histograma
+   de uma imagem colorida
+
+   Autor : Alex Alves
+*/   
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+
+int main(int argc, char** argv){
+
+    Mat image,hR,hG,hB,saida,cinza,sr,sb,sg;
+    image = imread(argv[1]);
+    vector<Mat> planes;
+    split (image, planes);
+    equalizeHist(planes[0],planes[0]);
+    equalizeHist(planes[1],planes[1]);
+    equalizeHist(planes[2],planes[2]);
+    merge(planes,saida);
+    
+    imshow("Saida",image);
+    imshow("Equalizar",saida);
+    imwrite("equalizado2.jpg",saida);
+    waitKey(0);
+   
+    return 0;
+}
